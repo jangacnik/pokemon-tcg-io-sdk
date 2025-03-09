51ddca5a4bb43145c1c200b09e89ac46840cc7c1
Jan Gacnik <nqmrnr7xgg@privaterelay.appleid.com>
2025-03-09 21:45:19 +0100

    Added query options

--- src/main/java/io/github/jangacnik/model/dto/GenericDto.java
+++ src/main/java/io/github/jangacnik/model/dto/GenericDto.java
@@ -3,14 +3,11 @@ package io.github.jangacnik.model.dto;
 import java.util.List;
 
 public class GenericDto {
-
   public List<String> getData() {
     return data;
   }
-
   public void setData(List<String> data) {
     this.data = data;
   }
-
   private List<String> data;
 }
